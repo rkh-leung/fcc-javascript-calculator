9800f6adaa4f5b8eee6583340d289045060a7d5e
rkh-leung <leptanian@gmail.com>
2021-08-09 17:23:28 +1200

    add: user story 8

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,26 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-    const [state, setState] = useState(0)
-
+    type stateType = number | string
+    const [state, setState] = useState<stateType>(0)
+    const [history, setHistory] = useState(0)
     const clearHandler = () => setState(0)
+    const inputHandler = (e: React.BaseSyntheticEvent) => {
+        setState(prevState  => {
+            if (prevState === 0) {
+                return Number(e.target.value)
+            } else {
+                return String(prevState).concat(e.target.value)
+            }
+        })
+    }
     return (
         <div className="App">
             <header className="App-header">
                 FCC JavaScript Calculator
             </header>
-            <div id={'display'}>{state}</div>
+            <div id={'result'}>{history}</div>
+            <input id={'display'} value={state}/>
             <div className={'input-Display'}>
                 <button className={'input-op'} id={'equals'}>{'\u003d'}</button>
                 <button className={'input-op'} id={'add'}>{'\u002b'}</button>
@@ -19,16 +30,66 @@ function App() {
                 <button className={'input-op'} id={'divide'}>{'\u00F7'}</button>
                 <button className={'input-op'} id={'decimal'}>{'\u002e'}</button>
                 <button className={'input-op'} id={'clear'} onClick={clearHandler}>{'\u0043'}</button>
-                <button className={'input-num'} id={'zero'}>0</button>
-                <button className={'input-num'} id={'one'}>1</button>
-                <button className={'input-num'} id={'two'}>2</button>
-                <button className={'input-num'} id={'three'}>3</button>
-                <button className={'input-num'} id={'four'}>4</button>
-                <button className={'input-num'} id={'five'}>5</button>
-                <button className={'input-num'} id={'six'}>6</button>
-                <button className={'input-num'} id={'seven'}>7</button>
-                <button className={'input-num'} id={'eight'}>8</button>
-                <button className={'input-num'} id={'nine'}>9</button>
+                <button className={'input-num'} id={'zero'}
+                        onClick={inputHandler}
+                        type={'submit'}
+                        value={0}
+                >0
+                </button>
+                <button className={'input-num'}
+                        id={'one'}
+                        onClick={inputHandler}
+                        type={'submit'}
+                        value={1}>1
+                </button>
+                <button className={'input-num'} id={'two'}
+                        onClick={inputHandler}
+                        type={'submit'}
+                        value={2}
+                >2
+                </button>
+                <button className={'input-num'} id={'three'}
+                        onClick={inputHandler}
+                        type={'submit'}
+                        value={3}
+                >3
+                </button>
+                <button className={'input-num'} id={'four'}
+                        onClick={inputHandler}
+                        type={'submit'}
+                        value={4}
+                >4
+                </button>
+                <button className={'input-num'} id={'five'}
+                        onClick={inputHandler}
+                        type={'submit'}
+                        value={5}
+                >5
+                </button>
+                <button className={'input-num'} id={'six'}
+                        onClick={inputHandler}
+                        type={'submit'}
+                        value={6}
+                >6
+                </button>
+                <button className={'input-num'} id={'seven'}
+                        onClick={inputHandler}
+                        type={'submit'}
+                        value={7}
+                >7
+                </button>
+                <button className={'input-num'} id={'eight'}
+                        onClick={inputHandler}
+                        type={'submit'}
+                        value={8}
+                >8
+                </button>
+                <button className={'input-num'} id={'nine'}
+                        onClick={inputHandler}
+                        type={'submit'}
+                        value={9}
+                >9
+                </button>
             </div>
         </div>
     );
