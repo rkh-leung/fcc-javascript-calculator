3f40eb15c863f591d0adfffe17c6fb69302df847
rkh-leung <leptanian@gmail.com>
2021-08-09 16:04:30 +1200

    add: user story 1 to 5

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+          FCC JavaScript Calculator
       </header>
+        <div className={'input-Display'}>
+            <button className={'input-op'} id={'equals'}>{'\u003d'}</button>
+            <button className={'input-op'} id={'add'}>{'\u002b'}</button>
+            <button className={'input-op'} id={'substract'}>{'\u2212'}</button>
+            <button className={'input-op'} id={'multiply'}>{'\u00D7'}</button>
+            <button className={'input-op'} id={'divide'}>{'\u00F7'}</button>
+            <button className={'input-op'} id={'decimal'}>{'\u002e'}</button>
+            <button className={'input-op'} id={'clear'}>{'\u0043'}</button>
+            <button className={'input-num'} id={'zero'}>0</button>
+            <button className={'input-num'} id={'one'}>1</button>
+            <button className={'input-num'} id={'two'}>2</button>
+            <button className={'input-num'} id={'three'}>3</button>
+            <button className={'input-num'} id={'four'}>4</button>
+            <button className={'input-num'} id={'five'}>5</button>
+            <button className={'input-num'} id={'six'}>6</button>
+            <button className={'input-num'} id={'seven'}>7</button>
+            <button className={'input-num'} id={'eight'}>8</button>
+            <button className={'input-num'} id={'nine'}>9</button>
+        </div>
     </div>
   );
 }
